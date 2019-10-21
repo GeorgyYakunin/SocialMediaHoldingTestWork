367d8e7397f4f0215d788f55514e64a61559881b
Georgy Yakunin <jrayg95@gmail.com>
2019-10-21 16:10:49 +0300

    bag fix

--- app/src/main/java/com/example/socialmediaholdingtestwork/PayActivity.java
+++ app/src/main/java/com/example/socialmediaholdingtestwork/PayActivity.java
@@ -2,8 +2,13 @@ package com.example.socialmediaholdingtestwork;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
 
 public class PayActivity extends AppCompatActivity {
+
+    EditText cardNumber;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -11,6 +16,72 @@ public class PayActivity extends AppCompatActivity {
         setContentView(R.layout.pay_activity);
         TypefaceUtil.overrideFont(getApplicationContext(), "SERIF", "fonts/Raleway-Bold.ttf");
 
+        cardNumber = findViewById(R.id.card_number_input);
+        cardNumber.addTextChangedListener(new TextWatcher() {
+
+            private static final int TOTAL_SYMBOLS = 19; // size of pattern 0000-0000-0000-0000
+            private static final int TOTAL_DIGITS = 16; // max numbers of digits in pattern: 0000 x 4
+            private static final int DIVIDER_MODULO = 5; // means divider position is every 5th symbol beginning with 1
+            private static final int DIVIDER_POSITION = DIVIDER_MODULO - 1; // means divider position is every 4th symbol beginning with 0
+            private static final char DIVIDER = '-';
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // noop
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // noop
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (!isInputCorrect(s, TOTAL_SYMBOLS, DIVIDER_MODULO, DIVIDER)) {
+                    s.replace(0, s.length(), buildCorrectString(getDigitArray(s, TOTAL_DIGITS), DIVIDER_POSITION, DIVIDER));
+                }
+            }
+
+            private boolean isInputCorrect(Editable s, int totalSymbols, int dividerModulo, char divider) {
+                boolean isCorrect = s.length() <= totalSymbols; // check size of entered string
+                for (int i = 0; i < s.length(); i++) { // check that every element is right
+                    if (i > 0 && (i + 1) % dividerModulo == 0) {
+                        isCorrect &= divider == s.charAt(i);
+                    } else {
+                        isCorrect &= Character.isDigit(s.charAt(i));
+                    }
+                }
+                return isCorrect;
+            }
+
+            private String buildCorrectString(char[] digits, int dividerPosition, char divider) {
+                final StringBuilder formatted = new StringBuilder();
+
+                for (int i = 0; i < digits.length; i++) {
+                    if (digits[i] != 0) {
+                        formatted.append(digits[i]);
+                        if ((i > 0) && (i < (digits.length - 1)) && (((i + 1) % dividerPosition) == 0)) {
+                            formatted.append(divider);
+                        }
+                    }
+                }
+
+                return formatted.toString();
+            }
+
+            private char[] getDigitArray(final Editable s, final int size) {
+                char[] digits = new char[size];
+                int index = 0;
+                for (int i = 0; i < s.length() && index < size; i++) {
+                    char current = s.charAt(i);
+                    if (Character.isDigit(current)) {
+                        digits[index] = current;
+                        index++;
+                    }
+                }
+                return digits;
+            }
+        });
 
     }
 }
